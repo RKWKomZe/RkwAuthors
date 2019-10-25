24de66371d3fef2f89521cab0d6137f227766d88
Steffen Kroggel <kroggel@rkw.de>
2019-10-25 13:37:18 +0200

    [REFACTORING] TCA reworking: return value for TCA

--- ext_tables.php
+++ ext_tables.php
@@ -35,6 +35,12 @@ if (!defined('TYPO3_MODE')) {
 );
 
 //=================================================================
+// Add Tables
+//=================================================================
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_rkwauthors_domain_model_authors', 'EXT:rkw_authors/Resources/Private/Language/locallang_csh_tx_rkwauthors_domain_model_authors.xlf');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_rkwauthors_domain_model_authors');
+
+//=================================================================
 // Add TypoScript
 //=================================================================
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'RKW Authors');
