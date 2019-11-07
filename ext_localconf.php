248b31d73ad534d80da1aa3d15f49155393f25f0
Steffen Kroggel <kroggel@rkw.de>
2019-11-07 12:26:51 +0100

    [REFACTORING] user_func for ext_localconf.php and ext_tables.php / fix for flashMessages

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,76 +1,80 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
-}
+defined('TYPO3_MODE') || die('Access denied.');
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'RKW.' . $_EXTKEY,
-	'Rkwauthors',
-	array(
-		'Authors' => 'list, pageBox, pageBoxSmallFirst, pageBoxSmallRest, pageSchemaOrg, pageCommaList',
-		'Ajax' => 'filter'
-		
-	),
-	// non-cacheable actions
-	array(
-		'Authors' => '',
+call_user_func(
+    function($extKey)
+    {
+        //=================================================================
+        // Configure Plugins
+        //=================================================================
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'RKW.' . $extKey,
+            'Rkwauthors',
+            array(
+                'Authors' => 'list, pageBox, pageBoxSmallFirst, pageBoxSmallRest, pageSchemaOrg, pageCommaList',
+                'Ajax' => 'filter'
+            ),
+            // non-cacheable actions
+            array(
+                'Authors' => '',
 
-		'Ajax' => ''
-		
-	)
+                'Ajax' => ''
+            )
+        );
+
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'RKW.' . $extKey,
+            'Rkwauthorsdetail',
+            array(
+                'Authors' => 'show',
+
+            ),
+            // non-cacheable actions
+            array(
+                'Authors' => 'show',
+            )
+        );
+
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'RKW.' . $extKey,
+            'Rkwauthorsheadline',
+            array(
+                'Authors' => 'headline',
+            ),
+            // non-cacheable actions
+            array(
+                'Authors' => 'headline',
+            )
+        );
+
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'RKW.' . $extKey,
+            'Rkwauthorswork',
+            array(
+                'Authors' => 'showWork',
+            ),
+            // non-cacheable actions
+            array(
+                'Authors' => 'showWork',
+            )
+        );
+
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'RKW.' . $extKey,
+            'Rkwauthorscontactbox',
+            array(
+                'Authors' => 'contactBox',
+            ),
+            // non-cacheable actions
+            array(
+                'Authors' => 'contactBox',
+            )
+        );
+        
+    },
+    $_EXTKEY
 );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'RKW.' . $_EXTKEY,
-	'Rkwauthorsdetail',
-	array(
-		'Authors' => 'show',
-
-	),
-	// non-cacheable actions
-	array(
-		'Authors' => 'show',
-	)
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'RKW.' . $_EXTKEY,
-	'Rkwauthorsheadline',
-	array(
-		'Authors' => 'headline',
-
-	),
-	// non-cacheable actions
-	array(
-		'Authors' => 'headline',
-	)
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'RKW.' . $_EXTKEY,
-	'Rkwauthorswork',
-	array(
-		'Authors' => 'showWork',
-
-	),
-	// non-cacheable actions
-	array(
-		'Authors' => 'showWork',
-	)
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'RKW.' . $_EXTKEY,
-	'Rkwauthorscontactbox',
-	array(
-		'Authors' => 'contactBox',
-
-	),
-	// non-cacheable actions
-	array(
-		'Authors' => 'contactBox',
-	)
-);
 
 // Add rootline-Fields
 $TYPO3_CONF_VARS['FE']['addRootLineFields'] .= ',tx_rkwauthors_authorship';
