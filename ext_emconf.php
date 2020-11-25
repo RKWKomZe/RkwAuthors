b1b6700ae56cedf0dc200c7ca48431b1844619f2
Steffen Kroggel <kroggel@rkw.de>
2020-11-25 09:14:23 +0100

    [FIX] Adding PlainEmailViewHelper in order to remove "mailto:"-prefix from schema.org

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = [
 	'constraints' => [
 		'depends' => [
             'typo3' => '8.7.0-8.7.99',
-            'rkw_basics' => '8.7.0-8.7.99',
+            'rkw_basics' => '8.7.53-8.7.99',
             'rkw_ajax' => '8.7.6-8.7.99',
 		],
 		'conflicts' => [
